17f2bcfb8331294ff947d1ccf2b434f9e395e573
Polat Öztürk <66801019+polatztrk@users.noreply.github.com>
2022-10-25 10:46:40 +0300

    Update node_motor.py
    Some issues solved

--- pkg_ros_motor/scripts/node_motor.py
+++ pkg_ros_motor/scripts/node_motor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 
 from std_msgs.msg import Int64
@@ -11,8 +11,13 @@ PWM_Output = 188
 pub = rospy.Publisher('/PWM_Values',Int16,queue_size=100)
 
 def pub_pwm():
-    pub.publish(PWM_Output)
+    r = rospy.Rate(1) # 1hz 
+    while not rospy.is_shutdown():
+        pub.publish(PWM_Output)
+        print("Publishing PWM value: {}".format(PWM_Output))
+        r.sleep()
 
 if __name__ == '__main__':
     print("Running")
+    rospy.init_node('pub')
     pub_pwm()
