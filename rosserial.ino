a7d859e808bec363420cf42ffddafe4ab2da18d4
Polat Öztürk <66801019+polatztrk@users.noreply.github.com>
2022-11-05 20:29:45 +0300

    Motor Controller Changed

--- rosserial.ino
+++ rosserial.ino
@@ -1,36 +1,46 @@
 
-// PINOUT
-// L_EN -> 8
-// R_EN -> 8
-// L_PWM -> 9
-// R_PWM -> 10
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include <WProgram.h>
+#endif
 
-#include "BTS7960.h"
 #include <ros.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int64.h>
 #include <std_msgs/Int16.h>
+#include <AFMotor.h>
+#include <std_msgs/UInt16.h>
 
 
 ros::NodeHandle  nh;
 
 
-const uint8_t EN = 8;
-const uint8_t L_PWM = 9;
-const uint8_t R_PWM = 10;
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
 
-BTS7960 motorController(EN, L_PWM, R_PWM);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
 
-void pwm_input( const std_msgs::Int16& pwm_value){
+AF_DCMotor motor3(3, MOTOR12_64KHZ);
+
+AF_DCMotor motor4(4, MOTOR12_64KHZ);
+
+void pwm_input( const std_msgs::UInt16& pwm_value){
   int pwm =0;
   pwm = pwm_value.data;
 
-  motorController.TurnLeft(pwm);
-  delay(100);
+  motor1.setSpeed(pwm);
+  motor2.setSpeed(pwm);
+  motor3.setSpeed(pwm);
+  motor4.setSpeed(pwm);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  delay(1000);
 
 }
 
-ros::Subscriber<std_msgs::Int16> pwm("PWM_Values", &pwm_input);
+ros::Subscriber<std_msgs::UInt16> pwm("PWM_Values", pwm_input);
 
 void setup()
 {
@@ -42,9 +52,9 @@ void setup()
 
 void loop()
 {
-  motorController.Enable();
+  
   nh.spinOnce();
-  delay(100);
+  delay(1);
 
 
 }
